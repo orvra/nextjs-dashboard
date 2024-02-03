dda034fcf31b953d952394deb08cf46c9f0ecdd5
orvra <135913026+orvra@users.noreply.github.com>
2024-02-02 21:38:13 -0500

    Initial commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Page() {
+  return <div>customer page</div>;
+}
+
+export default Page;
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,5 @@
+function Page() {
+  return <div>dashboard page</div>;
+}
+
+export default Page;
